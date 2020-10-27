e3b8e4f2342c081f9cb148562bc8e70ff8774bd0
Carlos Enrique Herrera <carlos@unfrikimas.com>
2020-10-27 18:09:24 +0100

    Creating login form

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,8 +1,49 @@
 import React from 'react';
 
 const Login = () => {
+
+    //funcion para el onChange
+    const onChange = () => {
+
+    }
+
     return (  
-        <h1>Login</h1>
+        
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Iniciar sesión</h1>
+                <form>
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input 
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Tu Email"
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input 
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Tu Password"
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <input 
+                            type="submit"
+                            className="btn btn-primario btn-block"
+                            value="Iniciar sesión"
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
+    
     );
 }
  
