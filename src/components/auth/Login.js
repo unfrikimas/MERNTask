a6ded8b058c59e2084001ce699c4c7009488cd68
Carlos Enrique Herrera <carlos@unfrikimas.com>
2020-10-27 18:23:55 +0100

    States and functions for Login component

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
 
-    //funcion para el onChange
-    const onChange = () => {
+    //State local para iniciar sesion
+    const [usuario, setUsuario] = useState({
+        email: '',
+        password: ''
+    });
 
+    //extraer del objeto usuario
+    const {email, password} = usuario;
+
+    //funcion para el onChange
+    const onChange = (e) => {
+        setUsuario({
+            ...usuario,
+            [e.target.name]: e.target.value
+        })
+    };
+
+    //funcion para el boton Iniciar Sesion
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        //validar
+
+        //Pasar los valores al action
     }
 
     return (  
@@ -12,7 +34,9 @@ const Login = () => {
         <div className="form-usuario">
             <div className="contenedor-form sombra-dark">
                 <h1>Iniciar sesión</h1>
-                <form>
+                <form
+                    onSubmit={onSubmit}
+                >
                     <div className="campo-form">
                         <label htmlFor="email">Email</label>
                         <input 
@@ -20,6 +44,7 @@ const Login = () => {
                             id="email"
                             name="email"
                             placeholder="Tu Email"
+                            value={email}
                             onChange={onChange}
                         />
                     </div>
@@ -30,6 +55,7 @@ const Login = () => {
                             id="password"
                             name="password"
                             placeholder="Tu Password"
+                            value={password}
                             onChange={onChange}
                         />
                     </div>
@@ -41,6 +67,11 @@ const Login = () => {
                         />
                     </div>
                 </form>
+
+                <Link to={'/nueva-cuenta'} className="enlace-cuenta">
+                    Crear cuenta
+                </Link>
+
             </div>
         </div>
     
