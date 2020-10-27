5b84371ad72a7872e65634bbf191c8ebb48714d9
Carlos Enrique Herrera <carlos@unfrikimas.com>
2020-10-27 20:03:15 +0100

    Creating new proyecto form

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import NuevoProyecto from '../proyectos/NuevoProyecto';
 
 const Sidebar = () => {
     return (  
         
         <aside>
             <h1>MERN<span>Task</span></h1>
+
+            <NuevoProyecto />
+
             <div className="proyectos">
                 <h2>Tus proyectos</h2>
             </div>
