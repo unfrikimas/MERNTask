028c7c1af1ede66ca17e9b7e34f7734dde615bb1
Carlos Enrique Herrera <carlos@unfrikimas.com>
2020-11-01 16:22:25 +0100

    Hide task form if there no projects

--- src/components/tareas/FormTarea.js
+++ src/components/tareas/FormTarea.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import proyectoContext from '../../context/proyectos/proyectoContext';
 
 const FormTarea = () => {
+
+    //obtener el proyecto activo
+    const proyectosContext = useContext(proyectoContext);
+    const {proyecto} = proyectosContext;
+
+    //Si no hay proyecto seleccionado
+    if(!proyecto) return null;
+    
+    //Array destructuring para extraer el proyecto actual
+    const [proyectoActual] = proyecto;    
+
     return (  
         
         <div className="formulario">
