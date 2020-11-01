7079b7645acf25c3977717d3d6dbafda50799652
Carlos Enrique Herrera <carlos@unfrikimas.com>
2020-11-01 16:11:53 +0100

    Show project choose on list

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -1,7 +1,22 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
 import Tarea from './Tarea';
 
+import proyectoContext from '../../context/proyectos/proyectoContext';
+
 const ListadoTareas = () => {
+
+    //obtener proyectos del state inicial
+    const proyectosContext = useContext(proyectoContext);
+    const {proyecto} = proyectosContext;
+
+    //Si no hay proyecto seleccionado
+    if(!proyecto) {
+        return (
+            <h2>Selecciona un proyecto</h2>
+        )        
+    }
+    //Array destructuring para extraer el proyecto actual
+    const [proyectoActual] = proyecto;
 
     const tareasProyecto = [
         {nombre: 'Elegir plataforma', estado: true},
@@ -13,7 +28,7 @@ const ListadoTareas = () => {
     return (  
         
         <Fragment>
-            <h2>Proyecto: Tienda virtual</h2>
+            <h2>Proyecto: {proyectoActual.nombre}</h2>
             <ul className="listado-tareas">
                 {tareasProyecto.length === 0
                     ? (<li className="tarea"><p>No hay tareas</p></li>)
