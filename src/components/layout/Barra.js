f5820fab83fdab83939acbbd260c7d9fa0e82220
Carlos Enrique Herrera <carlos@unfrikimas.com>
2020-11-16 19:53:57 +0100

    Mostrar informacion del usuario autenticado

--- src/components/layout/Barra.js
+++ src/components/layout/Barra.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import AuthContext from '../../context/autenticacion/authContext';
 
 const Barra = () => {
-    return (  
-        
-        <header className="app-header">
-            <p className="nombre-usuario">Hola <span>Carlos Enrique</span></p>
 
+    //Extraer la informacion de autenticacion 
+    const authContext = useContext(AuthContext);
+    const { usuario, usuarioAutenticado } = authContext;
+    
+    useEffect(() => {
+        usuarioAutenticado();
+    }, []);    
+
+    return (  
+        <header className="app-header">
+            { usuario ? <p className="nombre-usuario">Hola <span>{usuario.nombre}</span></p> : null }
             <nav className="nav-principal">
                 <a href="#!">Cerrar sesión</a>
             </nav>
         </header>
-
     );
 }
  
