656d4e338b9e9f7f218df5efc98abae5686495a4
Carlos Enrique Herrera <carlos@unfrikimas.com>
2020-11-19 21:25:57 +0100

    Ajustes para produccion

--- src/components/rutas/RutaPrivada.js
+++ src/components/rutas/RutaPrivada.js
@@ -9,6 +9,7 @@ const RutaPrivada = ({ component: Component, ...props }) => {
 
     useEffect(() => {
         usuarioAutenticado();
+        // eslint-disable-next-line
     }, []);
 
     return (
